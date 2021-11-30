0cdc9bc9081e0d90704b97b5257b955bcfcccc18
George Andredakis <george@andredakis.gr>
2021-11-30 20:51:46 +0200

    Deleted org.json:json dependency

--- src/main/java/com/project_christopher/libraries/srp/Components/M1AndA.java
+++ src/main/java/com/project_christopher/libraries/srp/Components/M1AndA.java
@@ -1,6 +1,5 @@
 package com.project_christopher.libraries.srp.Components;
 
-import org.json.JSONObject;
 
 public class M1AndA {
     public String A, M1;
@@ -8,11 +7,5 @@ public class M1AndA {
     public M1AndA(String a, String m1) {
         A = a;
         M1 = m1;
-    }
-
-    public JSONObject toJSON() {
-        return new JSONObject()
-                .put("A", this.A)
-                .put("M1", this.M1);
     }
 }
--- src/main/java/com/project_christopher/libraries/srp/Components/ServerState.java
+++ src/main/java/com/project_christopher/libraries/srp/Components/ServerState.java
@@ -1,6 +1,5 @@
 package com.project_christopher.libraries.srp.Components;
 
-import org.json.JSONObject;
 
 public class ServerState {
 
@@ -12,28 +11,5 @@ public class ServerState {
         this.verifier = verifier;
         this.B = B;
         this.b = b;
-    }
-
-    public JSONObject toJSON() {
-        return new JSONObject()
-                .put("identity", this.identity)
-                .put("salt", this.salt)
-                .put("verifier", this.verifier)
-                .put("B", this.B)
-                .put("b", this.b);
-    }
-
-    public static ServerState fromJSON(JSONObject json) {
-        return new ServerState(
-                json.has("identity") ? json.getString("identity") : "",
-                json.has("salt") ? json.getString("salt") : "",
-                json.has("verifier") ? json.getString("verifier") : "",
-                json.has("B") ? json.getString("B") : "",
-                json.has("b") ? json.getString("b") : ""
-        );
-    }
-
-    public static ServerState fromJSON(String json) {
-        return fromJSON(new JSONObject(json));
     }
 }
